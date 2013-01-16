9fb68308f19f53d868ccd8f7aa92cb8ec685ff03
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-01-16 19:25:55 -0200

    Separates the main.js to core.js and popup.js

--- javascripts/app/popup.js
+++ javascripts/app/popup.js
@@ -1,30 +1,3 @@
-RubyGems = function(name){
-  return {
-    query: name,
-    configs: {
-      search: "https://rubygems.org/search?query=",
-      api: "https://rubygems.org/api/v1/gems/"
-    },
-    exists: false,
-    info: {},
-    fetch: function(){
-      var that = this;
-      $.ajax({
-        url: this.configs.api + this.query + ".json",
-        error: function(){
-          that.exists = false;
-        },
-        success: function(data){
-          that.exists = true;
-          that.info = data;
-        }
-      });
-    }
-  }
-};
-
-
-
 $(document).ready(function(){
   chrome.tabs.getSelected(null, function(tab) {
     window.current_tab = tab
